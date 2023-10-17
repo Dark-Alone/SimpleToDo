c7bb5a7c320626051d8c8e265c72b4635ce95bf2
Mark Akivaev <mark220550@gmail.com>
2023-10-18 01:01:55 +0300

    Removed unecessary code for development

--- ToDo App/Persistence.swift
+++ ToDo App/Persistence.swift
@@ -12,11 +12,10 @@ struct PersistenceController {
 
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
+        let viewContext = result.viewContext
+        
+        
+        
         do {
             try viewContext.save()
         } catch {
@@ -29,6 +28,10 @@ struct PersistenceController {
     }()
 
     let container: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        container.viewContext
+    }
 
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "ToDo_App")
